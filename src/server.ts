8fe8d50e159ddc9b65de47e5f9d6410508e65533
JhonataAugust0 <jhon.augustosilva@gmail.com>
2024-04-04 13:26:45 -0300

    update: rota de health check

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
 import { errorHandler } from "./error-handler";
+import { health } from "./routes/health";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -45,9 +46,9 @@ app.register(getEvent)
 app.register(getAttendeeBadge)
 app.register(checkIn)
 app.register(getEventAttendees)
+app.register(health)
 
 app.setErrorHandler(errorHandler)
-
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
